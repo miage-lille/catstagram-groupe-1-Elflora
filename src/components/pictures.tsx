c42be944807a4e5cbf8f4cbe15781fa8127dd2e6
Carpentier Madeline <madeline.carpentier.etu@b12p19.fil.univ-lille.fr>
2025-02-04 17:02:29 +0100

    en cour

--- src/components/pictures.tsx
+++ src/components/pictures.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { getSelectedPicture, picturesSelector } from '../reducer';
 
 const Container = styled.div`
   padding: 1rem;
@@ -18,7 +20,40 @@ const Image = styled.img`
   }
 `;
 const Pictures = () => {
-  return null;
+  const pictures = useSelector(picturesSelector);
+  return (
+    <div>
+      {pictures.map((pic, index) => (
+        <img key={index} src={pic.previewFormat} alt={`Cat ${index}`} />
+      ))}
+    </div>
+  );
 };
+
+/*const Pictures = () => {
+  const dispatch = useDispatch();
+  const pictures = useSelector(picturesSelector);
+  const selectedPicture = useSelector(getSelectedPicture);
+
+  return (
+    <>
+      <Container>
+        {pictures.map((pic, index) => (
+          <Image key={index} src={pic.previewFormat} alt={`Cat ${index}`} onClick={() => dispatch(selectPicture(pic))} />
+        ))}
+      </Container>
+
+      {selectedPicture && (
+        <ModalContainer onClick={() => dispatch(closeModal())}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <img src={selectedPicture.largeFormat} alt="Large Cat" style={{ maxWidth: '100%' }} />
+            <p>Author: {selectedPicture.author}</p>
+            <CloseButton onClick={() => dispatch(closeModal())}>Close</CloseButton>
+          </ModalContent>
+        </ModalContainer>
+      )}
+    </>
+  );
+};*/
 
 export default Pictures;
